23cba8ae4d7eab7fa8e231b2465bc07fd34c25e1
swethaforwin-create <swethaforwin@gmail.com>
2026-09-21 22:05:24 +0530

    Implement sound playback using Web Audio API
    Refactored sound playback to use Web Audio API instead of Audio object.

--- sound.js
+++ sound.js
@@ -1,14 +1,26 @@
 document.addEventListener("click", function (event) {
 
-    if (event.target.tagName === "BUTTON" ||
-        event.target.closest("button")) {
+    if (event.target.closest("button")) {
 
-        let sound = new Audio();
+        const audio = new (window.AudioContext || window.webkitAudioContext)();
 
-        // Button click sound
-        sound.src = "https://actions.google.com/sounds/v1/ui/click.ogg";
+        const oscillator = audio.createOscillator();
+        const gain = audio.createGain();
 
-        sound.play();
+        oscillator.connect(gain);
+        gain.connect(audio.destination);
+
+        oscillator.frequency.value = 700;
+        oscillator.type = "sine";
+
+        gain.gain.setValueAtTime(0.2, audio.currentTime);
+        gain.gain.exponentialRampToValueAtTime(
+            0.01,
+            audio.currentTime + 0.15
+        );
+
+        oscillator.start();
+        oscillator.stop(audio.currentTime + 0.15);
     }
 
 });
